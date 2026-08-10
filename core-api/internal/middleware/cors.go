6efa2b9a02585686599b63509d302fcfd5e616bb
cqhasy <3181940782@qq.com>
2026-08-10 16:49:06 +0800

    fix(cors):use echo middleware

--- core-api/internal/middleware/cors.go
+++ core-api/internal/middleware/cors.go
@@ -1,42 +1,14 @@
 package middleware
 
 import (
-	"net/http"
-	"strings"
-
 	"github.com/labstack/echo/v4"
+	echomiddleware "github.com/labstack/echo/v4/middleware"
 )
-
-var allowedMethods = strings.Join([]string{
-	http.MethodGet,
-	http.MethodHead,
-	http.MethodPost,
-	http.MethodPut,
-	http.MethodPatch,
-	http.MethodDelete,
-	http.MethodOptions,
-}, ",")
 
 // CORS temporarily allows the API to be called from any origin.
 func CORS() echo.MiddlewareFunc {
-	return func(next echo.HandlerFunc) echo.HandlerFunc {
-		return func(c echo.Context) error {
-			request := c.Request()
-			headers := c.Response().Header()
-			headers.Add(echo.HeaderVary, echo.HeaderOrigin)
-			if request.Header.Get(echo.HeaderOrigin) == "" {
-				return next(c)
-			}
-
-			headers.Set(echo.HeaderAccessControlAllowOrigin, "*")
-
-			if request.Method != http.MethodOptions {
-				return next(c)
-			}
-
-			headers.Set(echo.HeaderAccessControlAllowMethods, allowedMethods)
-			headers.Set(echo.HeaderAccessControlAllowHeaders, echo.HeaderContentType)
-			return c.NoContent(http.StatusNoContent)
-		}
-	}
+	return echomiddleware.CORSWithConfig(echomiddleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{echo.HeaderContentType},
+	})
 }
